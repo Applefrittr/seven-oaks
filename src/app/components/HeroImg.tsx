726888de18dff83bb0bdcb69de6bf954e9e9f5d9
applefrittr <jacob.lee.jordan@gmail.com>
2024-10-23 14:11:45 -0400

    fix Image/next components in HeroImg.tsx

--- src/app/components/HeroImg.tsx
+++ src/app/components/HeroImg.tsx
@@ -15,8 +15,7 @@ function HeroImg({ img, alt, index, currIndex }: Img) {
       className={`${
         index === currIndex ? "opacity-100" : "opacity-0"
       } w-full h-full object-cover object-center absolute top-0 left-0 bottom-0 right-0 transition-opacity duration-[1500ms] ease-in-out`}
-      layout="fill"
-      objectFit="cover"
+      fill
       priority
     />
   );
