246adbbc77c8228e93a9a38ec558f6e99e056370
applefrittr <jacob.lee.jordan@gmail.com>
2024-12-13 11:52:10 -0500

    updated metaData obj in (public-site)/layout.tsx, dimension change to icon.ico

--- src/app/(public-site)/layout.tsx
+++ src/app/(public-site)/layout.tsx
@@ -3,8 +3,27 @@ import Sidebar from "../components/Sidebar";
 import WelcomePopup from "../components/WelcomePopup";
 
 export const metadata: Metadata = {
-  title: "Seven Oaks",
+  title: "Seven Oaks - Arnaudville",
   description: "Luxury living in Arnaudville, LA",
+  keywords: ["Seven Oaks", "Arnaudville", "Lousianna", "LA"],
+  authors: { name: "applefrittr" },
+  openGraph: {
+    title: "Seven Oaks - Arnaudville",
+    description: "Luxury living in Arnaudville, LA",
+    url: process.env.HOST_URL,
+    siteName: "Seven Oaks - Arnaudville",
+    images: {
+      url: `${process.env.HOST_URL}/SO-logo.png`,
+      width: 800,
+      height: 525,
+      alt: "Seven Oaks Logo",
+    },
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function Layout({
