f0c323fd278a8aa76143726da945d88793b273bc
applefrittr <jacob.lee.jordan@gmail.com>
2024-12-13 12:07:09 -0500

    updates #1 to metaData object in (public-site)/layout.tsx

--- src/app/(public-site)/layout.tsx
+++ src/app/(public-site)/layout.tsx
@@ -3,17 +3,17 @@ import Sidebar from "../components/Sidebar";
 import WelcomePopup from "../components/WelcomePopup";
 
 export const metadata: Metadata = {
-  title: "Seven Oaks - Arnaudville",
+  title: "Seven Oaks of Arnaudville",
   description: "Luxury living in Arnaudville, LA",
   keywords: ["Seven Oaks", "Arnaudville", "Lousianna", "LA"],
   authors: { name: "applefrittr" },
   openGraph: {
-    title: "Seven Oaks - Arnaudville",
+    title: "Seven Oaks of Arnaudville",
     description: "Luxury living in Arnaudville, LA",
     url: process.env.HOST_URL,
-    siteName: "Seven Oaks - Arnaudville",
+    siteName: "Seven Oaks",
     images: {
-      url: `${process.env.HOST_URL}/SO-logo.png`,
+      url: `${process.env.HOST_URL}SO-logo.png`,
       width: 800,
       height: 525,
       alt: "Seven Oaks Logo",
