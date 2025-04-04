9f320f017baa4875177c0b7343145b8ec829ad5d
applefrittr <jacob.lee.jordan@gmail.com>
2025-04-04 14:51:20 -0400

    LoadingSpinner component -> added to LoginForm

--- src/app/(admin-dashboard)/login/page.tsx
+++ src/app/(admin-dashboard)/login/page.tsx
@@ -1,11 +1,9 @@
-//import CreateUserForm from "./CreateUserForm";
 import LoginForm from "./LoginForm";
 
 export default function Login() {
   return (
     <section className={`w-96`}>
       <LoginForm />
-      {/* <CreateUserForm /> */}
     </section>
   );
 }
